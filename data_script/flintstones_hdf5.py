6bc30a65b2338afcc1b180aeef3520435ebf56af
tang584 <meng.tang@pnnl.gov>
2023-12-15 10:44:10 -0800

    use contig vlen

--- data_script/flintstones_hdf5.py
+++ data_script/flintstones_hdf5.py
@@ -8,6 +8,21 @@ import h5py
 import numpy as np
 from tqdm import tqdm
 
+import time
+
+import os
+def get_chunk_shape():
+    chunk_shape = os.getenv("CHUNK_NUM")
+    if chunk_shape == None:
+        print("CHUNK_NUM is not a valid integer, set to false")
+        # chunk_shape = None
+    else:
+        # chunk_shape = (int(chunk_shape),)
+        # chunk_shape = (1,int(chunk_shape))
+        chunk_shape = int(chunk_shape)
+    
+    print("Chunk shape:", chunk_shape)
+    return chunk_shape
 
 def main(args):
     splits = json.load(open(os.path.join(args.data_dir, 'train-val-test_split.json'), 'r'))
@@ -17,7 +32,10 @@ def main(args):
     descriptions = dict()
     for sample in annotations:
         descriptions[sample["globalID"]] = sample["description"]
-
+    
+    # Get the current time in seconds since the epoch (January 1, 1970)
+    start_time_seconds = time.time()
+    
     f = h5py.File(args.save_path, "w")
     for subset, ids in {'train': train_ids, 'val': val_ids, 'test': test_ids}.items():
         ids = [i for i in ids if i in followings and len(followings[i]) == 4]
@@ -26,8 +44,8 @@ def main(args):
         group = f.create_group(subset)
         images = list()
         for i in range(5):
-            images.append(
-                group.create_dataset('image{}'.format(i), (length,), dtype=h5py.vlen_dtype(np.dtype('uint8'))))
+            images.append(group.create_dataset('image{}'.format(i), (length,), dtype=h5py.vlen_dtype(np.dtype('uint8'))))
+            # images.append(group.create_dataset('image{}'.format(i), (length,), dtype=h5py.vlen_dtype(np.dtype('uint8')), chunks=get_chunk_shape()))
         text = group.create_dataset('text', (length,), dtype=h5py.string_dtype(encoding='utf-8'))
         for i, item in enumerate(tqdm(ids, leave=True, desc="saveh5")):
             globalIDs = [item] + followings[item]
@@ -41,7 +59,16 @@ def main(args):
                 txt.append(descriptions[globalID])
             text[i] = '|'.join([t.replace('\n', '').replace('\t', '').strip() for t in txt])
     f.close()
-
+    
+    end_time_seconds = time.time()
+    print("Start time:", start_time_seconds)
+    print("End time:", end_time_seconds)
+    # Calculate the time difference
+    time_elapsed = end_time_seconds - start_time_seconds
+    # Calculate minutes and seconds from the time difference
+    minutes_elapsed = int(time_elapsed // 60)
+    seconds_elapsed = int(time_elapsed % 60)
+    print(f"Time elapsed: {time_elapsed:.2f} seconds ({minutes_elapsed} minutes and {seconds_elapsed} seconds)")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='arguments for flintstones hdf5 file saving')
